dcbc1afcde8e0f49847aa95b5e92751a072f3e55
Max_H <rwmaherr@gmail.com>
2025-05-17 21:44:30 +0200

    Testing workflow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use rostig::run;
 
-#[cfg(target_arch="wasm32")]
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-
-#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 fn main()
 {
     run();
